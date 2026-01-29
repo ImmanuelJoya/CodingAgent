1ccc33072196da2081c4e42199634825995935fb
Immanuel Joy <immanueljoy107@gmail.com>
2026-01-29 15:46:35 -0500

    implemented verbose

--- main.py
+++ main.py
@@ -16,18 +16,31 @@ def main():
     if len(sys.argv) <2:
         print ("Please provide a prompt as a command line argument.")
         sys.exit(1)
+    
+    verbose_flag = False
+    if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+        print ("Verbose mode enabled.")
+        verbose_flag = True    
 
     prompt = sys.argv[1]
     
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    
     response = client.models.generate_content(
         model = "gemini-2.5-flash",
-        contents = prompt,
+        contents = messages,
     )
     print(response.text)
     if response is None or response.usage_metadata is None:
         print("No response or usage metadata available.")
         return
-    print (f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print (f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    
+    if verbose_flag:
+        
+        print (f"User Prompt: {prompt}")
+        print (f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print (f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 
 main()
