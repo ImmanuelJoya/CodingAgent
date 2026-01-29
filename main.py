6738c453c289201643b9b1e08cc0268345a283db
Immanuel Joy <immanueljoy107@gmail.com>
2026-01-29 10:31:08 -0500

    implemented genai-api

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from agentcode!")
+import os 
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMIMNI_API_KEY")
+client = genai.Client(api_key =api_key)
+
+print (f"API Key: {api_key}")
+
+response = client.models.generate_content(
+    model = "gemini-2.5-flash",
+    contents = "name a few sifi-movies of all times, based on users reviews",
+)
+
+print(response.text)
