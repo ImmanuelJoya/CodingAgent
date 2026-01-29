8dd0a3da1f3af1f7022f48776bdacd9abd984eb2
Immanuel Joy <immanueljoy107@gmail.com>
2026-01-29 11:34:19 -0500

    implemented genai-api

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from google.genai import types
 
 
 load_dotenv()
-api_key = os.environ.get("GEMIMNI_API_KEY")
+api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key =api_key)
 
-print (f"API Key: {api_key}")
+# print (f"API Key: {api_key}")
 
 response = client.models.generate_content(
     model = "gemini-2.5-flash",
