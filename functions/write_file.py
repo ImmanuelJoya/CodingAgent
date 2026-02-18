9a82ed4073abcdd46842a55f459bb51705733eae
Immanuel Joy <immanueljoy107@gmail.com>
2026-02-17 19:21:17 -0500

    updated

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,9 +1,30 @@
 import os
 
-def write_file(working_directory: str, directory: str, file_path, content):
+def write_file(working_directory: str, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     if not abs_file_path.startswith(abs_working_dir):
         return f"Error: File path {file_path} is outside the working directory {abs_working_dir}."
+    
+    parent_dir = os.path.dirname(abs_file_path)
+    if not os.path.exists(parent_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f"Error creating directories for {file_path}: {str(e)}"  
+
     if not os.path.isfile(abs_file_path):
-        return f"Error: File {file_path} does not exist."
+        pass
+        # parent_dir = os.path.dirname(abs_file_path)
+        # try:
+        #     os.makedirs(parent_dir)
+        # except Exception as e:
+        #     return f"Error creating directories for {file_path}: {str(e)}"   
+    try:
+        with open(abs_file_path, 'w') as f:
+            f.write(content)
+        return f"File {file_path} written successfully."
+    except Exception as e:
+        return f"Error writing to file {file_path}: {str(e)}"             
+    except Exception as e:
+        return f"Error writing to file {file_path}: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
-from functions.get_files_info import get_files_info 
-from functions.get_file_content import get_file_content
+# from functions.get_files_info import get_files_info 
+# from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def main():
     working_dir = "calculator"
-    print(get_file_content(working_dir, "main.py"))
-    print(get_file_content(working_dir, "pkg/calculator.py"))
-    print(get_file_content(working_dir, "/bin/cat"))
+    # print(write_file(working_dir, "lorem.txt", "wait, this is a test!!"))
+    print (write_file(working_dir, "pkg/morelorem.txt" , "hey wait, this is a test!"))
+    # print (write_file(working_dir, "tmp/temp.txt", "this souldn't be allowed!"))
+
+    # print(get_file_content(working_dir, "main.py"))
+    # print(get_file_content(working_dir, "pkg/calculator.py"))
+    # print(get_file_content(working_dir, "/bin/cat"))
     
 main()
